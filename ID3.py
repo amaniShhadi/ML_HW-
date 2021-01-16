46f497af1268be4555bdb5ae76536c7331992c3a
Amani Shhadi <amani.shhadi@gmail.com>
2021-01-16 16:13:55 +0200

    new implementation to get binary tree

--- ID3.py
+++ ID3.py
@@ -1,30 +1,16 @@
-import AbstractClassifier
 import math
 import pandas as pd
 import numpy as np
-
 from collections import deque
 
 
-def entropy(col):
-    diff_values = sorted(col.unique())
-    num_diff_values = len(diff_values)
-
-    normalized_counts = col.value_counts(bins=num_diff_values-1, sort=False, normalize=True)
-    res = sum([-count * math.log(count, 2)
-               if count else 0
-               for count in normalized_counts])
-    return res
-
-
 class ClassifyNode:
-    def __init__(self, data_ids, feature_id, vals_range):
+    def __init__(self, data_ids, feature_id, feature_bin, majority):
         self.data_ids = data_ids
         self.feature_id = feature_id
-        self.children = []
-        self.majority = None #calc_majority()
-        self.parent = None
-        self.vals_range = vals_range
+        self.children = []  # binary array
+        self.majority = majority
+        self.feature_bin = feature_bin
 
 
 class ID3_Classifier:
@@ -34,6 +20,7 @@ class ID3_Classifier:
         self.features = features
         self.root = None
 
+    # todo: call build tree function
     def fit(self, x, y):
         """abstract function
            check what is the x, y
@@ -47,81 +34,89 @@ class ID3_Classifier:
         assert self.root is not None
         return self.predict_aux(self.root, data)
 
-    def predict_aux(self, node, data):
+    def predict_aux(self, node, data_line):
         if not node.children or len(node.children) == 0:
             return node.majority
 
-        for child in node.children:
-            if data[child.feature_id] in child.vals_range:
-                return self.predict_aux(child, data)
+        if data_line[node.feature_id] < node.feature_bin:
+            return self.predict_aux(node.children[0], data_line)
+        else:
+            return self.predict_aux(node.children[1], data_line)
 
-
-
+    def calc_majority(self, data_ids):
+        outputs = [self.data_output[id] for id in data_ids]
+        positive_cnt = len([x for x in outputs if (x == 1)])
+        negative_cnt = len([x for x in outputs if (x == 0)])
+        if positive_cnt < negative_cnt:
+            return 0
+        return 1
 
     def calc_entropy(self, data_ids):
-        # sorted labels by instance id
-        output = [self.data_output[i] for i in data_ids]
-        # count number of instances of each category
-        output_count = [output.count(x) for x in [0, 1]]
-        # calculate the entropy for each category and sum them
-        entropy = sum([-count / len(data_ids) * math.log(count / len(data_ids), 2) if count else 0 for count in output_count])
+        # calc the outputs the of the data_ids
+        outputs = [self.data_output[i] for i in data_ids]
+        # count the number of the different outputs
+        output_count = [outputs.count(x) for x in [0, 1]]
+
+        entropy = 0
+        for count in output_count:
+            if count:
+                p_i = count / len(data_ids)
+                entropy += -p_i * math.log(p_i, 2)
         return entropy
 
-    def cal_feature_vals_ranges(self, data_ids, feature_id):
-        x_features = [self.data.iloc[x][feature_id] for x in data_ids]
-        # get unique values
-        feature_vals = list(set(x_features))
-        # if not feature_vals:
-        #     print("f vals ---->: ", feature_vals)
-        #     print("feature_id:", feature_id)
-        #     print("data_ids:",data_ids)
-        #
-        #     exit(0)
+    def cal_feature_vals_bins(self, data_ids, feature_id):
+        feature_vals = sorted(list(set([self.data.iloc[x][feature_id] for x in data_ids])))
+        if len(feature_vals) == 1:
+            return [feature_vals[0] + 1]
 
-        bins = 1 if len(feature_vals) < 2 else len(feature_vals) - 1
-        feature_vals_ranges = pd.unique(pd.cut(np.array(feature_vals), bins, right=False, ordered=True, duplicates='drop'))
-        # print("f vals ---->: ",feature_vals)
-        # print("------->feature_vals_ranges:")
-        # print(feature_vals_ranges)
+        bins = []
+        for i in range(len(feature_vals)-1):
+            bins.append( feature_vals[i] + ((feature_vals[i+1] - feature_vals[i]) / 2))
 
-        return feature_vals_ranges
+        return bins
 
     def calc_information_gain(self, data_ids, feature_id):
         # calculate total entropy
         curr_IG = self.calc_entropy(data_ids)
-        # store in a list all the values of the chosen feature
-        # print("--------> feature_id  ", feature_id)
-        # print("--------> data_ids  ", data_ids)
-        # print("----amani ", self.data.iloc[0][0])
-        # exit(0)
+        feature_vals_bins = self.cal_feature_vals_bins(data_ids, feature_id)
 
-        feature_vals_ranges = self.cal_feature_vals_ranges(data_ids, feature_id)
-        feature_vals_ranges_ids = []
-
+        max_info_gain = float('-inf')
+        best_bin = float('-inf')
         # get frequency of each value
-        for val_range in feature_vals_ranges:
-            val_ids = []
+        for bin in feature_vals_bins:
+            smaller_ids = []
+            bigger_ids = []
+
             for id in data_ids:
-                if self.data.iloc[id][feature_id] in val_range:
-                    val_ids.append(id)
-            feature_vals_ranges_ids.append(val_ids)
+                if self.data.iloc[id][feature_id] < bin:
+                    smaller_ids.append(id)
+                else:
+                    bigger_ids.append(id)
+            # compute the information gain with the chosen feature
+            smaller_entropy = len(smaller_ids)/len(data_ids) * self.calc_entropy(smaller_ids)
+            bigger_entropy = len(bigger_ids)/len(data_ids) * self.calc_entropy(bigger_ids)
+            info_gain = curr_IG - smaller_entropy - bigger_entropy
+            if info_gain >= max_info_gain:
+                max_info_gain = info_gain
+                best_bin = bin
+        return max_info_gain, best_bin
 
-        # compute the information gain with the chosen feature
-        entropy_sum = 0
-        for i in range(len(feature_vals_ranges_ids)):
-            entropy_sum += len(feature_vals_ranges_ids[i])/len(data_ids) * self.calc_entropy(feature_vals_ranges_ids[i])
-
-        return curr_IG - entropy_sum
 
     def get_best_feature_id(self, data_ids, feature_ids):
         # for each feature calc entropy
-        features_info_gain = [self.calc_information_gain(data_ids, feature_id) for feature_id in feature_ids]
-        # find the feature that maximises the information gain
-        # print("amanaaaaaaaaani 27sn w7de! ")
-        # print(features_info_gain)
-        best_id = feature_ids[features_info_gain.index(max(features_info_gain))]
+        features_info_gain = []
+        for feature_id in feature_ids:
+            features_info_gain.append(self.calc_information_gain(data_ids, feature_id))
 
-        return best_id
+        best_indx = 0
+        best_info_gain = features_info_gain[0][0]
+
+        for i in range(len(features_info_gain)):
+            if features_info_gain[i][0] >= best_info_gain:
+                best_indx = i
+                best_info_gain = features_info_gain[i][0]
+
+        return feature_ids[best_indx], features_info_gain[best_indx][1]
 
 
     def build_id3_tree(self):
@@ -135,56 +130,46 @@ class ID3_Classifier:
         feature_ids = [x for x in range(len(self.features))]
         assert feature_ids is not None
 
-        best_feature_id = self.get_best_feature_id(data_ids, feature_ids)
-        self.root = ClassifyNode(data_ids, best_feature_id, (float('-inf'), float('inf')))
+        best_feature_id, best_feature_bin = self.get_best_feature_id(data_ids, feature_ids)
+
+        self.root = ClassifyNode(data_ids, best_feature_id, best_feature_bin, self.calc_majority(data_ids))
         self.build_id3_tree_aux(self.root, data_ids, feature_ids)
 
-    def calc_majority(self, data_ids):
-        outputs = [self.data_output[id] for id in data_ids]
-        positive_cnt = len([x for x in outputs if (x == 1)])
-        negative_cnt = len([x for x in outputs if (x == 0)])
-        if positive_cnt < negative_cnt:
-            return 0
-        return 1
-
     def build_id3_tree_aux(self, root, data_ids, feature_ids):
+        if len(data_ids) == 0:
+            return ClassifyNode(data_ids, None, None, root.majority)
+
         # sorted labels by instance id
         outputs = [self.data_output[id] for id in data_ids]
 
         # if all the example have the same class (pure node), return node
         if len(set(outputs)) == 1:
-            node = ClassifyNode(data_ids, None, None)
-            node.majority = self.calc_majority(data_ids)
+            node = ClassifyNode(data_ids, None, None, self.calc_majority(data_ids))
             return node
 
-        # if there are not more feature to compute, return node with the most probable class
-        # if len(feature_ids) == 0:
-        #     root.value = max(set(labels_in_features), key=labels_in_features.count)  # compute mode
-        #     return root
-        # else...
-        # choose the feature that maximizes the information gain
-        best_feature_id = self.get_best_feature_id(data_ids, feature_ids)
-        self.root.feature_id = best_feature_id
+        best_feature_id, best_feature_bin = self.get_best_feature_id(data_ids, feature_ids)
+        root.feature_id = best_feature_id
+        root.feature_bin = best_feature_bin
 
-        feature_vals_ranges = self.cal_feature_vals_ranges(data_ids, best_feature_id)
+        left_child_ids = []
+        right_child_ids = []
 
-        # value of the chosen feature for each instance
-        # feature_values = list(set([self.data.iloc[x][best_feature_id] for x in data_ids]))
-        # loop through all the values
+        for x in data_ids:
+            if self.data.iloc[x][best_feature_id] < best_feature_bin:
+                left_child_ids.append(x)
+            else:
+                right_child_ids.append(x)
 
+        left_child = ClassifyNode(left_child_ids, None, None, self.calc_majority(left_child_ids))
+        root.children.append(left_child)
 
-        for vals_range in feature_vals_ranges:
-            child_data_ids = [x for x in data_ids if self.data.iloc[x][best_feature_id] in vals_range]
-            if child_data_ids is None or len(child_data_ids) == 0:
-                continue
+        right_child = ClassifyNode(right_child_ids, None, None, self.calc_majority(right_child_ids))
+        root.children.append(right_child)
 
-            child = ClassifyNode(child_data_ids, best_feature_id, vals_range)
-            child.majority = self.calc_majority(child_data_ids)
-            root.children.append(child)
-
-            # todo check with Sammar
-            # self._id3_recv(child_data_ids, feature_ids, child)
-            self.build_id3_tree_aux(child, child_data_ids, feature_ids )
+        # todo check with Sammar
+        # self._id3_recv(child_data_ids, feature_ids, child)
+        self.build_id3_tree_aux(left_child, left_child_ids, feature_ids)
+        self.build_id3_tree_aux(right_child, right_child_ids, feature_ids)
 
 
     def printTree(self):
@@ -198,10 +183,9 @@ class ID3_Classifier:
             print("node address = ", node)
             print("child.feature_id", '({})'.format(node.feature_id))
             print("child.data_ids", '({})'.format(node.data_ids))
+            print("child.bin", '({})'.format(node.feature_bin))
             print("child.children", '({})'.format(node.children))
-            print("child.vals_range", '({})'.format(node.vals_range))
             print("child.majority", '({})'.format(node.majority))
-
 
             if node.children:
                 # print("children: ")
--- main.py
+++ main.py
@@ -3,88 +3,10 @@ import pandas as pd
 import math
 from ID3 import ID3_Classifier
 
-from collections import deque
-import sklearn, random, matplotlib
-
-""" :parameter df: data frame
-    :return the entropy of the given data frame"""
-# def calc_entropy(df):
-#     output_col = df[df.columns[-1]]
-#     diff_values = sorted(output_col.unique())
-#     num_diff_values = len(diff_values)
-#     # print("----    num_diff_values  ", num_diff_values)
-#     # total_cnt = col.count()
-#     # print("---- total_cnt  ", total_cnt)
-#     frequency_table = output_col.value_counts(bins=num_diff_values, sort=False, normalize=True)
-#     # print("+++++++++++> ranges")
-#     # print(frequency_table)
-#     res = sum([-count * math.log(count, 2)
-#                if count else 0
-#                for count in frequency_table])
-#     return res
-
-
-def calc_IG(df, feature):
-    curr_entropy = calc_entropy(df)
-
-    feature_col = df[feature]
-    diff_values = sorted(feature_col.unique())
-    print(diff_values)
-    print("newwww")
-    print( df[(df[feature] > 1)])
-    # normalized_counts = feature_col.value_counts(bins=num_diff_values-1, sort=False, normalize=True)
-    aa = pd.qcut(feature_col, q=len(diff_values) -1)
-    # print(aa)
-
-    # num_diff_values = len(diff_values)
-    # # print("----", diff_values)
-    # # total_cnt = col.count()
-    #
-    # frequency_table = col.value_counts(bins=num_diff_values - 1, sort=False, normalize=True)
-    # # print("+++++++++++> ranges")
-    # # print(normalized_counts)
-    # res = sum([-count * math.log(count, 2)
-    #            if count else 0
-    #            for count in frequency_table])
-    # return res
-
-
-def _get_information_gain(self, x_ids, feature_id):
-    """Calculates the information gain for a given feature based on its entropy and the total entropy of the system.
-    Parameters
-    __________
-    :param x_ids: list, List containing the instances ID's
-    :param feature_id: int, feature ID
-    __________
-    :return: info_gain: float, the information gain for a given feature.
-    """
-    # calculate total entropy
-    info_gain = self._get_entropy(x_ids)
-    # store in a list all the values of the chosen feature
-    x_features = [self.X[x][feature_id] for x in x_ids]
-    # get unique values
-    feature_vals = list(set(x_features))
-    # get frequency of each value
-    feature_v_count = [x_features.count(x) for x in feature_vals]
-    # get the feature values ids
-    feature_v_id = [
-        [x_ids[i]
-        for i, x in enumerate(x_features)
-        if x == y]
-        for y in feature_vals
-    ]
-
-    # compute the information gain with the chosen feature
-    info_gain_feature = sum([v_counts / len(x_ids) * self._get_entropy(v_ids)
-                             for v_counts, v_ids in zip(feature_v_count, feature_v_id)])
-
-    info_gain = info_gain - info_gain_feature
-    return info_gain
-
 
 if __name__ == "__main__":
 
-    data = pd.read_csv("train.csv")
+    data = pd.read_csv("amani.csv")
     feature_names = list(data.keys())[:-1]
     data_features = np.array(data.drop('Outcome', axis=1).copy())
     data_outcome = np.array(data['Outcome'].copy())
@@ -102,86 +24,19 @@ if __name__ == "__main__":
     id3_tree.build_id3_tree()
     id3_tree.printTree()
 
-    test = pd.read_csv("test.csv")
-    test_features = np.array(data.drop('Outcome', axis=1).copy())
-    test_outcome = np.array(data['Outcome'].copy())
+    test = pd.read_csv("amani_test.csv")
+    test_features = np.array(test.drop('Outcome', axis=1).copy())
+    test_outcome = np.array(test['Outcome'].copy())
     match_cnt = 0
+    none_cnt = 0
     for i in range(len(test_features)):
         res = id3_tree.predict(test_features[i])
         print ("res : ", res, "  predict : ", test_outcome[i])
         if res == test_outcome[i]:
             match_cnt += 1
+        if res is None:
+            none_cnt += 1
 
     print("percision = ", match_cnt/len(test_features))
+    print("none_cnt  = ", none_cnt)
 
-
-
-
-
-    # df = np.genfromtxt('amani.csv', delimiter=',', dtype=None)
-    # print(X[0][1])
-    # # # load csv file
-    # features = df.columns
-    # #
-    # # # for row in labels.iterrows():
-    # # #     print(row[0])
-    # # #     print("***************")
-    # # #
-    # # # for i in range(len(labels.index)):
-    # # #     print(labels.loc[i, [features[0]]])
-    # # #     print("&&&&&&&&&&&&")
-    # # #
-    # # # for f in features:
-    # # #     print("=======   " + f)
-    # # #
-    # # # print("-------------> features")
-    # # # print(features)
-    # # # print("-------------> features")
-    # #
-    # # """array of arrays where each array has the unique value of that feature
-    # # """
-    # # all_ranges = [sorted(df[x].unique()) for x in features]
-    # # # labelCategoriesCount = sorted(df.Pregnancies.unique())
-    # # print(all_ranges)
-    # # print(features)
-    # # # for feature in features:
-    # # # for limit in labelCategoriesCount[0]:
-    # # #     print(df[df[features[0]] <= limit ].count()[0])
-    #
-    # res = calc_IG(df, features[0])
-    # # first_col = df[df.columns[-1]]
-    # # # print(first_col)
-    # # #
-    # # res = calc_entropy(df)
-    # print("[[[[[[[[[[[[[[[[[[[[[[[[[[[", res, "]]]]]]]]]]]]]]]]]]]]]]")
-    #
-    # # total_attributes = first_col.count()
-    # # # ranges = first_col.groupby(pd.cut(df[features[0]], all_ranges[0])).count()
-    # # # print("777777   -----> ", all_ranges[0])
-    # # ranges_2 = first_col.value_counts(bins=all_ranges[0], sort=False)
-    # # print(ranges_2)
-    # # # print("yyyyyyyyyyyyyyyyyyyyyy")
-    # # print("total_attributes =  ", total_attributes)
-    # # entropy = sum([-count / total_attributes * math.log(count / total_attributes, 2)
-    # #                if count else 0
-    # #                for count in ranges_2])
-    # #
-    # # print("entropy  =   ", entropy)
-    #
-    # # ent = 0
-    # # for count in ranges_2:
-    # #     ent += -count / total_attributes * math.log(count / total_attributes, 2)
-    # #     print("---> ", count)
-    # # print("ent  =   ", ent)
-    #
-    #
-    # # print(first_col.groupby(pd.cut(df[features[0]], ranges[0])).count())
-    # # print("====", df[df.Outcome == 1].count()[1])
-    # # true = df[df.Outcome == 1].count()[1]
-    # # print ("===1==  ", true)
-    # # false = df[df.Outcome == 0].count()[1]
-    # # print ("===0==  ", false)
-    # #
-    # # res = true > false
-    # # print ("....res", res)
-    # # print(sorted(df.Pregnancies.unique()))
